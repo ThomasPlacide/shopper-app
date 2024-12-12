d7144278714bde0110f1db5a45be4fea557bc2c2
thmsplcd <47633912+ThomasPlacide@users.noreply.github.com>
2024-12-12 23:30:58 +0100

    Initial Commit

---
+++ shopper-app/shopper_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopper_appApp.swift
+//  shopper-app
+//
+//  Created by Thomas Placide on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct shopper_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shopper-appTests/shopper_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  shopper_appTests.swift
+//  shopper-appTests
+//
+//  Created by Thomas Placide on 12/12/2024.
+//
+
+import Testing
+@testable import shopper_app
+
+struct shopper_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
